ebd57167ee925ce7850ef79564f49fd9f9e33415
Valery <valera.nedbai.developer@gmail.com>
2020-10-10 09:16:21 +0300

    pods init

---
+++ Pods/Target Support Files/Pods-app_VK-app_VKUITests/Pods-app_VK-app_VKUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_app_VK_app_VKUITests : NSObject
+@end
+@implementation PodsDummy_Pods_app_VK_app_VKUITests
+@end
---
+++ Pods/Target Support Files/Pods-app_VK/Pods-app_VK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_app_VK : NSObject
+@end
+@implementation PodsDummy_Pods_app_VK
+@end
